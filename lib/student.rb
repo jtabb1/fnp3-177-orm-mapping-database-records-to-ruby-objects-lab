56b0cb02407ef9cc4d53470b7066a42dd1553bb2
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-07-19 22:51:57 -0400

    passed 7 tests so far

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,45 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  def initialize(name: nil, grade: nil, id: nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
+
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    # student = Student.new
+    # student.id = row[0]
+    # student.name = row[1]
+    # student.grade = row[2]
+    self.new(id: row[0], name: row[1], grade: row[2])
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+    SQL
+    DB[:conn].execute(sql).map do |rw| 
+      self.new_from_db(rw)
+    end
   end
 
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |rw|
+      self.new_from_db(rw)
+    end.first
   end
   
   def save
@@ -20,7 +47,6 @@ class Student
       INSERT INTO students (name, grade) 
       VALUES (?, ?)
     SQL
-
     DB[:conn].execute(sql, self.name, self.grade)
   end
   
@@ -32,7 +58,6 @@ class Student
       grade TEXT
     )
     SQL
-
     DB[:conn].execute(sql)
   end
 
